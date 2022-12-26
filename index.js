2ee67a81a853ee2066f2cf630a27b09eff3eae61
Matthew Okadinya <matthewokadinya@gmail.com>
2022-12-26 13:29:50 +0100

    extension practice

---
+++ index.js
@@ -0,0 +1,39 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+// log out "button clicked!" when the user clicks the "save input" button
+const inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function() { 
+// push the value from the inputEl into myLeads array 
+// we have to get value from the input field 
+    myLeads.push(inputEl.value)
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
