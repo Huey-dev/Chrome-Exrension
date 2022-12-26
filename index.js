e48e5b806c84c982570e288171d078319893715d
Matthew Okadinya <matthewokadinya@gmail.com>
2022-12-26 16:52:36 +0100

    i dont know what is wrong with my code

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ inputBtn.addEventListener("click", function() {
 // push the value from the inputEl into myLeads array 
 // we have to get value from the input field 
     myLeads.push(inputEl.value)
+    // call the renderLeads() function
+    renderLeads()
 }) 
-
-
-
+// grab the unordered list and store it in a const variable called ulEl
+const ulEl = document.getElementById("ul-el")
+// wrap all the codes in a renderLeads() function
+function renderLeads() {
+        // a variable to hold all the html for the list items
+    let listItems = ""
+    // render the leads in the unordered list using ulEl.textContent
+    // add the items to the listitems variable instead of the ulEl.innerHTML
+    // ulEl.innerHTML +="<li>" + myLeads[i] + "</li>"  
+    for(let i = 0; i = myLeads.length; i++) {
+        listItems +="<li>" + myLeads[i] + "</li>"  
+    }
+    // render the listItems inside the unordered list using ulEl.innerHTML
+    ulEl.innerHTML = listItems
+}
 
 
 
